022f9b78a911d7e1948622fe82aed49b7f794651
Mihail Markovski <mihailmarkovski@gmail.com>
2025-11-14 12:59:16 +0100

    refactor: introduce strategy pattern with ItemUpdater base class
    - Add ItemUpdater abstract base class with update() method and helper methods
    - Add StandardItemUpdater that implements all existing complex logic
    - Add get_updater_for() function that returns StandardItemUpdater for all items
    - Refactor GildedRose.update_quality() to use strategy pattern
    - Move helper methods (decrease_sell_in, increase_quality, etc.) to ItemUpdater base
    - No behavior change - all existing tests pass

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 
-class GildedRose(object):
-
-    def __init__(self, items):
-        self.items = items
-
+class ItemUpdater:
+    """Base class for item update strategies"""
+    
+    def update(self, item):
+        """Update the item. To be implemented by subclasses."""
+        raise NotImplementedError("Subclasses must implement update method")
+    
     def decrease_sell_in(self, item, amount=1):
         """Decrease sell_in by the specified amount"""
         item.sell_in = item.sell_in - amount
@@ -21,35 +23,59 @@ class GildedRose(object):
         """Set quality to 0"""
         item.quality = 0
 
-    def update_quality(self):
-        for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        self.decrease_quality(item, 1)
+
+class StandardItemUpdater(ItemUpdater):
+    """Handles updates for all items using the original complex logic"""
+    
+    def update(self, item):
+        # First phase: handle quality changes before sell_in update
+        if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
+            if item.quality > 0:
+                if item.name != "Sulfuras, Hand of Ragnaros":
+                    self.decrease_quality(item, 1)
+        else:
+            if item.quality < 50:
+                self.increase_quality(item, 1)
+                if item.name == "Backstage passes to a TAFKAL80ETC concert":
+                    if item.sell_in < 11:
+                        if item.quality < 50:
+                            self.increase_quality(item, 1)
+                    if item.sell_in < 6:
+                        if item.quality < 50:
+                            self.increase_quality(item, 1)
+        
+        # Update sell_in
+        if item.name != "Sulfuras, Hand of Ragnaros":
+            self.decrease_sell_in(item, 1)
+        
+        # Second phase: handle quality changes after sell_in update (after sell date)
+        if item.sell_in < 0:
+            if item.name != "Aged Brie":
+                if item.name != "Backstage passes to a TAFKAL80ETC concert":
+                    if item.quality > 0:
+                        if item.name != "Sulfuras, Hand of Ragnaros":
+                            self.decrease_quality(item, 1)
+                else:
+                    self.set_quality_to_zero(item)
             else:
                 if item.quality < 50:
                     self.increase_quality(item, 1)
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                self.increase_quality(item, 1)
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                self.increase_quality(item, 1)
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                self.decrease_sell_in(item, 1)
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                self.decrease_quality(item, 1)
-                    else:
-                        self.set_quality_to_zero(item)
-                else:
-                    if item.quality < 50:
-                        self.increase_quality(item, 1)
+
+
+def get_updater_for(item):
+    """Return the appropriate updater for the given item"""
+    return StandardItemUpdater()
+
+
+class GildedRose(object):
+
+    def __init__(self, items):
+        self.items = items
+
+    def update_quality(self):
+        for item in self.items:
+            updater = get_updater_for(item)
+            updater.update(item)
 
 
 class Item:
